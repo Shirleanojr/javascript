e61ee3a67bb2e17b3bf86cd5d56737ad1eba7159
SHIRLEANO DACIO JUNIOR <shirleanojr@gmail.com>
2024-05-19 22:50:25 -0300

    Atualização aula 11- condições

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var velocidade = 60.5;
+
+console.log(`A velocidade do seu carro é ${velocidade}Km/h`);
+
+if (velocidade > 60) {
+    console.log('Você ultrapassou a velocidade permitira. MULTADO!');
+}
+
+console.log('Dirija sempre usando cinto de segurança!');
